90f804c6a67b7109bcbfaf37222b3feb10e2a9f2
renzooUp <renzoupiachihua3@outlook.com>
2022-07-10 00:44:53 -0500

    Mi primer proyecto

---
+++ src/logeoproyect/hiloHora.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logeoproyect;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import javax.swing.JOptionPane;
+
+public class hiloHora {
+    int hora;
+    int minuto;
+    
+    //constructor
+    public hiloHora(int hora, int minuto ) {
+
+        this.hora = hora;
+        this.minuto = minuto;
+    }
+    public void run() {
+        boolean estado = true;
+         while (estado) {
+
+            // estoy capturando la hora del sistema
+            Calendar fechaSis = new GregorianCalendar();
+
+            int horaSis = fechaSis.get(Calendar.HOUR_OF_DAY);
+            int minutoSis = fechaSis.get(Calendar.MINUTE);
+        
+             if (hora == horaSis && minuto == minutoSis) {
+                    //mostramos un mensaje a la hora que suene la alarma
+
+                    JOptionPane.showMessageDialog(null, "Hora de tomar su medicamento\n");
+                    estado= false;
+
+            }
+
+        }
+    } public void start() {
+        new Thread((Runnable) this).start();
+    }
+}
+
